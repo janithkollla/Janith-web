54d4be400bd373c1b012b50aaa0fcbee80a9977e
janithkollla <janithpab2003@gmail.com>
2025-06-08 20:23:42 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for sidebar links
+document.querySelectorAll('.sidebar a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
